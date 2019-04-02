7531a3f644ab1045eb7068c36951a54636f3a045
Dan Cox <daniel.57@Gmail.com>
2019-04-01 22:22:20 -0400

    Fixed Yargs issues
    Extwee will now demand that the "output" (o) argument be supplied as both decompile and compile use it.

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
-/*
- *	 
- *    
- */
-
-const util = require('util');
-
 const argv = require('yargs')
-	.usage('Usage: extwee -i [Source] -f [Story Format] -d [Decompile File] -o [Output file]')
-	.describe('i', 'Source')
-    .describe('h', 'Help')
+    .describe('i', 'Source')
     .describe('v', 'Version')
     .describe('f', 'Story Format')
     .describe('d', 'File to decompile')
     .describe('o', 'Output File')
-    .alias('h', 'help')
     .alias('v', 'version')
     .alias('i', 'input')
     .alias('f', 'format')
     .alias('d', 'decompile')
     .alias('o', 'output')
+    .alias('h', 'help')
+    .demandOption(['o'])
     .argv;
-
+    
 const TweeParser = require('./TweeParser.js');
 const TweeWriter = require('./TweeWriter.js');
 const StoryFormatParser = require('./StoryFormatParser.js');
 const HTMLParser = require('./HTMLParser.js');
 const HTMLWriter = require('./HTMLWriter.js');
 
-let tp = null;
-let sfp = null;
+if(argv.hasOwnProperty("input") ) {
 
-if(argv.hasOwnProperty("input")) {
+    if(argv.hasOwnProperty("format") ) {
 
-    tp = new TweeParser(argv.input);
-    sfp = new StoryFormatParser(argv.format);
-    let hw = new HTMLWriter(argv.output, tp.story, sfp.JSON);
-    //console.log(util.inspect(tp.story, { showHidden: true, depth: null, colors: true }));
+        if(argv.hasOwnProperty("output") ) {
 
-}
+            let tp = new TweeParser(argv.input);
+            let sfp = new StoryFormatParser(argv.format);
+            let hw = new HTMLWriter(argv.output, tp.story, sfp.JSON);
 
-if(argv.hasOwnProperty("decompile")) { 
+        } else {
 
-    let hd = new HTMLParser(argv.decompile);
-    let tw = new TweeWriter(hd.story, argv.output);
-    //console.log(util.inspect(hd.passages, { showHidden: true, depth: null, colors: true }));
+            throw new Error("Missing output file!");
 
+        }
+
+    } else {
+
+        throw new Error("Missing format file");
+
+    }
+} else if(argv.hasOwnProperty("decompile") ) { 
+
+    if(argv.hasOwnProperty("output") )
+    {
+        let hd = new HTMLParser(argv.decompile);
+        let tw = new TweeWriter(hd.story, argv.output);
+
+    } else {
+
+        throw new Error("Missing output file!");
+
+    }
 }
 
