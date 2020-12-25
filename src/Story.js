df69aae856dcd0cccef014068b3abdd782561d48
Dan Cox <dan.cox@videlais.com>
2020-12-24 21:51:03 -0500

    2.0: Story

--- src/Story.js
+++ src/Story.js
@@ -1,69 +1,386 @@
 import { name, version } from '../package.json';
-import Passage from './Passage';
-
+import Passage from './Passage.js';
 /**
  * @class Story
  * @module Story
  */
 class Story {
   /**
+   * Name
+   *
+   * @private
+   */
+  #_name = 'Unknown';
+
+  /**
+   * Internal IFID
+   *
+   * @private
+   */
+  #_IFID = ''
+
+  /**
+   * Internal story format
+   *
+   * @private
+   */
+  #_format = '';
+
+  /**
+   * Internal version of story format
+   */
+  #_formatVersion = '';
+
+  /**
+   * Internal zoom level
+   */
+  #_zoom = '';
+
+  /**
+   * Internal state metadata
+   *
+   * @private
+   */
+  #_start = null;
+
+  /**
+   * Passages
+   *
+   * @private
+   */
+  #_passages = [];
+
+  /**
+   * UserScript Passage
+   *
+   * @private
+   */
+  #_scriptPassage = null;
+
+  /**
+   * UserStylesheet Passage
+   *
+   * @private
+   */
+  #_stylesheetPassage = null;
+
+  /**
+   * StoryTitle Passage
+   *
+   * @private
+   */
+  #_storyTitlePassage = null;
+
+  /**
+   * StoryAuthor Passage
+   *
+   * @private
+   */
+  #_storyAuthorPassage = null;
+
+  /**
+   * Creator
+   *
+   * @private
+   */
+  #_creator = '';
+
+  /**
+   * CreatorVersion
+   *
+   * @private
+   */
+  #_creatorVersion = '';
+
+  /**
    * @function Story
    * @class
    */
   constructor () {
-    // Name of the story
-    this.name = 'Unknown';
-
-    // Story metadata
-    this.metadata = {
-      ifid: '',
-      format: '',
-      formatVersion: '',
-      zoom: '',
-      start: null
-    };
-
-    // Internal passages
-    this.passages = [];
-
-    // Store the user script separately
-    this.scriptPassage = null;
-
-    // Store the user stylesheet separately
-    this.stylesheetPassage = null;
-
     // Store the creator and version
-    this.creator = name;
-    this.creatorVersion = version;
+    this.#_creator = name;
+    this.#_creatorVersion = version;
+  }
+
+  /**
+   * Interactive Fiction ID (IFID) of Story
+   *
+   * @public
+   * @readonly
+   * @memberof Story
+   * @returns {string} IFID
+   */
+  get IFID () { return this.#_IFID; }
+
+  /**
+   * @param {string} i - Replacement IFID
+   */
+  set IFID (i) {
+    if (typeof i === 'string') {
+      this.#_IFID = i;
+    } else {
+      throw new Error('IFID must be a String!');
+    }
+  }
+
+  /**
+   * Story format version of Story
+   *
+   * @public
+   * @readonly
+   * @memberof Story
+   * @returns {string} story format version
+   */
+  get formatVersion () { return this.#_formatVersion; }
+
+  /**
+   * @param {string} f - Replacement format version
+   */
+  set formatVersion (f) {
+    if (typeof f === 'string') {
+      this.#_formatVersion = f;
+    } else {
+      throw new Error('Story format version must be a String!');
+    }
+  }
+
+  /**
+   * Story format of Story
+   *
+   * @public
+   * @readonly
+   * @memberof Story
+   * @returns {string} format
+   */
+  get format () { return this.#_format; }
+
+  /**
+   * @param {string} f - Replacement format
+   */
+  set format (f) {
+    if (typeof f === 'string') {
+      this.#_format = f;
+    } else {
+      throw new Error('Story format must be a String!');
+    }
+  }
+
+  /**
+   * Program used to create Story
+   *
+   * @public
+   * @readonly
+   * @memberof Story
+   * @returns {string} Creator Program
+   */
+  get creator () { return this.#_creator; }
+
+  /**
+   * Version used to create Story
+   *
+   * @public
+   * @readonly
+   * @memberof Story
+   * @returns {string} Version
+   */
+  get creatorVersion () { return this.#_creatorVersion; }
+
+  /**
+   * Name
+   *
+   * @public
+   * @memberof Story
+   * @returns {string} Name
+   */
+  get name () { return this.#_name; }
+
+  /**
+   * @param {string} n - Replacement name
+   */
+  set name (n) {
+    if (typeof n === 'string') {
+      this.#_name = n;
+    } else {
+      throw new Error('Name must be a String!');
+    }
+  }
+
+  /**
+   * Zoom level
+   *
+   * @public
+   * @memberof Story
+   * @returns {string} Zoom level
+   */
+  get zoom () { return this.#_zoom; }
+
+  /**
+   * @param {string} n - Replacement zoom level
+   */
+  set zoom (n) {
+    if (typeof n === 'string') {
+      this.#_zoom = n;
+    } else {
+      throw new Error('Zoom level must be a String!');
+    }
+  }
+
+  /**
+   * Start passage
+   *
+   * @public
+   * @memberof Story
+   * @returns {Passage|null} Passage or null
+   */
+  get start () { return this.#_start; }
+
+  /**
+   * @param {Passage} p - Replacement start passage
+   */
+  set start (p) {
+    if (p instanceof Passage) {
+      this.#_start = p;
+    } else {
+      throw new Error('Start passage must be an instance of Passage!');
+    }
+  }
+
+  /**
+   * UserScript passage
+   *
+   * @public
+   * @memberof Story
+   * @returns {Passage|null} Passage or null
+   */
+  get scriptPassage () { return this.#_scriptPassage; }
+
+  /**
+   * @param {Passage} p - Replacement UserScript passage
+   */
+  set scriptPassage (p) {
+    if (p instanceof Passage) {
+      this.#_scriptPassage = p;
+    } else {
+      throw new Error('UserScript passage must be an instance of Passage!');
+    }
+  }
+
+  /**
+   * UserStylesheet passage
+   *
+   * @public
+   * @memberof Story
+   * @returns {Passage|null} Passage or null
+   */
+  get stylesheetPassage () { return this.#_stylesheetPassage; }
+
+  /**
+   * @param {Passage} p - Replacement UserStylesheet passage
+   */
+  set stylesheetPassage (p) {
+    if (p instanceof Passage) {
+      this.#_stylesheetPassage = p;
+    } else {
+      throw new Error('UserStylesheet passage must be an instance of Passage!');
+    }
+  }
+
+  /**
+   * StoryTitle passage
+   *
+   * @public
+   * @memberof Story
+   * @returns {Passage|null} Passage or null
+   */
+  get storyTitlePassage () { return this.#_storyTitlePassage; }
+
+  /**
+   * @param {Passage} p - Replacement UserStylesheet passage
+   */
+  set storyTitlePassage (p) {
+    if (p instanceof Passage) {
+      this.#_storyTitlePassage = p;
+    } else {
+      throw new Error('StoryTitle passage must be an instance of Passage!');
+    }
+  }
+
+  /**
+   * StoryAuthor passage
+   *
+   * @public
+   * @memberof Story
+   * @returns {Passage|null} Passage or null
+   */
+  get storyAuthorPassage () { return this.#_storyAuthorPassage; }
+
+  /**
+   * @param {Passage} p - Replacement UserStylesheet passage
+   */
+  set storyAuthorPassage (p) {
+    if (p instanceof Passage) {
+      this.#_storyAuthorPassage = p;
+    } else {
+      throw new Error('StoryAuthor passage must be an instance of Passage!');
+    }
   }
 
   /**
    * Add a passage to the story
    *
+   * @public
    * @function addPassage
+   * @memberof Story
    * @param {Passage} p - Passage to add to Story
    */
   addPassage (p) {
-    // Check if passed argument is a PAssage
+    // Check if passed argument is a Passage
     if (p instanceof Passage) {
-      // Push the passage to the array
-      this.passages.push(p);
+      // Does this passage already exist in the collection?
+      if (this.getPassageByName(p.name) === null) {
+        // Push the passage to the array
+        this.#_passages.push(p);
+      } else {
+        // Warn user
+        console.warn('Ignored passage with same name as existing one!');
+      }
     } else {
-      // Throw an error
+      // We can only add passages to array
       throw new Error('Can only add Passages to the story!');
     }
   }
 
   /**
-   * Find passages by tags
+   * Remove a passage from the story
    *
+   * @public
+   * @function removePassage
+   * @memberof Story
+   * @param {Passage} p - Passage to remove
+   */
+  removePassage (p) {
+    // Check if passed argument is a Passage
+    if (p instanceof Passage) {
+      // Filter out any passages with the same hash as argument
+      this.#_passages = this.#_passages.filter((passage) => passage.hash !== p.hash);
+    } else {
+      // We can only remove passages
+      throw new Error('Must pass Passage to compare!');
+    }
+  }
+
+  /**
+   * Find passages by tag
+   *
+   * @public
    * @function getPassagesByTag
+   * @memberof Story
    * @param {string} t - Passage name to search for
    * @returns {Array} Return array of passages
    */
-  getPassagesByTag (t = '') {
+  getPassagesByTag (t) {
     // Look through passages
-    return this.passages.filter((passage) => {
+    return this.#_passages.filter((passage) => {
       // Look through each passage's tags
       return passage.tags.some((tag) => t === tag);
     });
@@ -72,63 +389,46 @@ class Story {
   /**
    * Find passage by name
    *
+   * @public
    * @function getPassageByName
+   * @memberof Story
    * @param {string} name - Passage name to search for
-   * @returns {object | null} Return passage or null
+   * @returns {Passage | null} Return passage or null
    */
-  getPassageByName (name = '') {
+  getPassageByName (name) {
     // Look through passages
-    const results = this.passages.filter((passage) => passage.name === name);
-    // Return first entry or null, if not found
-    return results.length > 0 ? results[0] : null;
+    const results = this.#_passages.find((passage) => passage.name === name);
+    // Return entry or null, if not found
+    return results !== undefined ? results : null;
   }
 
   /**
    * Find passage by PID
    *
+   * @public
    * @function getPassageByPID
+   * @memberof Story
    * @param {number} pid - Passage PID to search for
-   * @returns {object | null} Return passage or null
+   * @returns {Passage | null} Return passage or null
    */
-  getPassageByPID (pid = -1) {
+  getPassageByPID (pid) {
     // Look through passages
-    const results = this.passages.filter((passage) => passage.pid === pid);
-    // Return first entry or null, if not found
-    return results.length > 0 ? results[0] : null;
+    const results = this.#_passages.find((passage) => passage.pid === pid);
+    // Return passages or null
+    return results !== undefined ? results : null;
   }
 
   /**
-   * Find the starting passages
+   * Size (number of passages).
+   * Does not include StoryAuthor, StoryTitle, UserScript, or UserStylesheet passages
    *
-   * @function getStartingPassage
-   * @returns {string|null} Return passage name or null
+   * @public
+   * @function size
+   * @memberof Story
+   * @returns {number} Return number of passages
    */
-  getStartingPassage () {
-    let pid = null;
-
-    // Is there a start property?
-    if (Object.prototype.hasOwnProperty.call(this.metadata, 'start')) {
-      // Check if the property is null
-      if (this.metadata.start !== null) {
-        // Look for the passage by name
-        const result = this.getPassageByName(this.metadata.start);
-        // If the search found something, save the PID
-        if (result !== null) {
-          pid = result.pid;
-        }
-      }
-    } else {
-      // Since there was not a 'start' property, look for Start passage
-      const results = this.getPassageByName('Start');
-      // Did we get any results?
-      if (results !== null) {
-        // Save the first one
-        pid = results.pid;
-      }
-    }
-
-    // We either found it or this will be null
-    return pid;
+  size () {
+    return this.#_passages.length;
   }
 }
 
--- test/Story.test.js
+++ test/Story.test.js
@@ -1,60 +1,288 @@
 import Story from '../src/Story.js';
 import Passage from '../src/Passage';
+import { name, version } from '../package.json';
 
 describe('Story', function () {
   describe('#constructor()', function () {
-    test('Should have default values', function () {
+    test('Should have extwee name', function () {
       const s = new Story();
-      expect(s.name).toBe('Unknown');
+      expect(s.creator).toBe(name);
+    });
+
+    test('Should have extwee version', function () {
+      const s = new Story();
+      expect(s.creatorVersion).toBe(version);
     });
   });
 
-  describe('#getPassageByName()', function () {
-    test('Should return Passage based on name', function () {
+  describe('IFID', function () {
+    test('Set using String', function () {
       const s = new Story();
-      const p = new Passage('Name');
+      s.IFID = 'New';
+      expect(s.IFID).toBe('New');
+    });
+
+    test('Should throw error if not String', function () {
+      const s = new Story();
+      expect(() => {
+        s.IFID = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('name', function () {
+    test('Set using String', function () {
+      const s = new Story();
+      s.name = 'New';
+      expect(s.name).toBe('New');
+    });
+
+    test('Should throw error if not String', function () {
+      const s = new Story();
+      expect(() => {
+        s.name = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('format', function () {
+    test('Set using String', function () {
+      const s = new Story();
+      s.format = 'New';
+      expect(s.format).toBe('New');
+    });
+
+    test('Should throw error if not String', function () {
+      const s = new Story();
+      expect(() => {
+        s.format = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('formatVersion', function () {
+    test('Set using String', function () {
+      const s = new Story();
+      s.formatVersion = 'New';
+      expect(s.formatVersion).toBe('New');
+    });
+
+    test('Should throw error if not String', function () {
+      const s = new Story();
+      expect(() => {
+        s.formatVersion = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('zoom', function () {
+    test('Set using String', function () {
+      const s = new Story();
+      s.zoom = 'New';
+      expect(s.zoom).toBe('New');
+    });
+
+    test('Should throw error if not String', function () {
+      const s = new Story();
+      expect(() => {
+        s.zoom = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('start', function () {
+    test('Set using Passage', function () {
+      const s = new Story();
+      const p = new Passage();
+      s.start = p;
+      expect(s.start instanceof Passage).toBe(true);
+    });
+
+    test('Should throw error if not Passage', function () {
+      const s = new Story();
+      expect(() => {
+        s.start = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('scriptPassage', function () {
+    test('Set using Passage', function () {
+      const s = new Story();
+      const p = new Passage();
+      s.scriptPassage = p;
+      expect(s.scriptPassage instanceof Passage).toBe(true);
+    });
+
+    test('Should throw error if not Passage', function () {
+      const s = new Story();
+      expect(() => {
+        s.scriptPassage = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('stylesheetPassage', function () {
+    test('Set using Passage', function () {
+      const s = new Story();
+      const p = new Passage();
+      s.stylesheetPassage = p;
+      expect(s.stylesheetPassage instanceof Passage).toBe(true);
+    });
+
+    test('Should throw error if not Passage', function () {
+      const s = new Story();
+      expect(() => {
+        s.stylesheetPassage = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('storyTitlePassage', function () {
+    test('Set using Passage', function () {
+      const s = new Story();
+      const p = new Passage();
+      s.storyTitlePassage = p;
+      expect(s.storyTitlePassage instanceof Passage).toBe(true);
+    });
+
+    test('Should throw error if not Passage', function () {
+      const s = new Story();
+      expect(() => {
+        s.storyTitlePassage = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('storyAuthorPassage', function () {
+    test('Set using Passage', function () {
+      const s = new Story();
+      const p = new Passage();
+      s.storyAuthorPassage = p;
+      expect(s.storyAuthorPassage instanceof Passage).toBe(true);
+    });
+
+    test('Should throw error if not Passage', function () {
+      const s = new Story();
+      expect(() => {
+        s.storyAuthorPassage = 1;
+      }).toThrow();
+    });
+  });
+
+  describe('addPassage()', function () {
+    test('addPassage() - should increase size', function () {
+      const s = new Story();
+      const p = new Passage();
       s.addPassage(p);
-      expect(s.getPassageByName('Name').name).toBe('Name');
+      expect(s.size()).toBe(1);
     });
 
-    test('Should return null if not found', function () {
+    test('addPassage() - should throw error if non-Passage', function () {
       const s = new Story();
-      const p = new Passage('Name');
-      s.passages.push(p);
-      expect(s.getPassageByName('Another')).toBe(null);
+      expect(() => {
+        s.addPassage(null);
+      }).toThrow();
     });
 
-    test('Should return null with default parameter', function () {
+    test('addPassage() - should prevent passages with same name being added', function () {
       const s = new Story();
-      expect(s.getPassageByName()).toBe(null);
-    });
-  });
-
-  describe('#getPassageByPID()', function () {
-    test('Should return Passage based on name', function () {
-      const s = new Story();
-      const p = new Passage('Name', [], {}, '', 12);
-      s.passages.push(p);
-      expect(s.getPassageByPID(12).pid).toBe(12);
-    });
-
-    test('Should return null if not found', function () {
-      const s = new Story();
-      const p = new Passage('Name', [], {}, '', 12);
-      s.passages.push(p);
-      expect(s.getPassageByPID(1)).toBe(null);
-    });
-
-    test('Should return null with default parameter', function () {
-      const s = new Story();
-      expect(s.getPassageByPID()).toBe(null);
+      const p = new Passage('A');
+      const p2 = new Passage('A');
+      s.addPassage(p);
+      s.addPassage(p2);
+      expect(s.size()).toBe(1);
     });
   });
 
-  describe('#getStartingPassage()', function () {
-    test('Should be null if no passages exist', function () {
+  describe('removePassage()', function () {
+    test('removePassage() - should decrease size', function () {
       const s = new Story();
-      expect(s.getStartingPassage()).toBe(null);
+      const p = new Passage('Find');
+      s.addPassage(p);
+      const passage = s.getPassageByName('Find');
+      s.removePassage(passage);
+      expect(s.size()).toBe(0);
+    });
+
+    test('removePassage() - should throw error if non-Passage', function () {
+      const s = new Story();
+      expect(() => {
+        s.removePassage(null);
+      }).toThrow();
+    });
+  });
+
+  describe('getPassagesByTag()', function () {
+    test('getPassagesByTag() - should find passages', function () {
+      const s = new Story();
+      const p = new Passage('Find', ['one']);
+      const p2 = new Passage('Find2', ['one']);
+      s.addPassage(p);
+      s.addPassage(p2);
+      const ps = s.getPassagesByTag('one');
+      expect(ps).toHaveLength(2);
+    });
+
+    test('getPassagesByTag() - should find none if none in collection', function () {
+      const s = new Story();
+      const ps = s.getPassagesByTag('one');
+      expect(ps).toHaveLength(0);
+    });
+
+    test('getPassagesByTag() - should find none if no tags match search', function () {
+      const s = new Story();
+      const p = new Passage('Find', ['one']);
+      const p2 = new Passage('Find2', ['one']);
+      s.addPassage(p);
+      s.addPassage(p2);
+      const ps = s.getPassagesByTag('two');
+      expect(ps).toHaveLength(0);
+    });
+  });
+
+  describe('getPassageByName()', function () {
+    test('getPassageByName() - should get passage by name', function () {
+      const s = new Story();
+      const p = new Passage('Find');
+      s.addPassage(p);
+      const passage = s.getPassageByName('Find');
+      expect(passage.name).toBe('Find');
+    });
+
+    test('removePassage() - should return null if not found', function () {
+      const s = new Story();
+      expect(s.getPassageByName('Find')).toBe(null);
+    });
+  });
+
+  describe('getPassageByPID()', function () {
+    test('getPassageByPID() - should get passage by PID', function () {
+      const s = new Story();
+      const p = new Passage('Find', [], {}, '', 12);
+      s.addPassage(p);
+      const passage = s.getPassageByPID(12);
+      expect(passage.name).toBe('Find');
+    });
+
+    test('getPassageByPID() - should return null if not found', function () {
+      const s = new Story();
+      expect(s.getPassageByPID(12)).toBe(null);
+    });
+  });
+
+  describe('size()', function () {
+    test('size() - should report number of passages', function () {
+      // Create a Story
+      const s = new Story();
+      // Create a Passage
+      const p = new Passage('');
+      // Test initial size
+      expect(s.size()).toBe(0);
+      // Add a passage
+      s.addPassage(p);
+      // Test size after adding one
+      expect(s.size()).toBe(1);
     });
   });
 });
