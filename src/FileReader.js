2344c454e193ed4695091921413190e8c243d56b
Dan Cox <daniel.57@Gmail.com>
2023-09-08 14:19:40 -0400

    Adding readBinaryAsBuffer function

--- src/FileReader.js
+++ src/FileReader.js
@@ -5,29 +5,55 @@ import fs from 'fs';
  */
 export default class FileReader {
   /**
-   * Read a text file
+   * Read a text file.
    *
    * @static
    * @public
-   * @function readFile
+   * @function read
    * @memberof FileReader
    * @param {string} file - Path of file to read
    * @returns {string} Content of file
    */
   static read (file) {
-    // Setup default value
+    // Setup default value.
     let contents = '';
 
-    // Attempt to find the file
+    // Attempt to find the file.
     if (fs.existsSync(file)) {
       // The file exists.
       contents = fs.readFileSync(file, 'utf8');
     } else {
-      // Throw error if file does not exist
+      // Throw error if file does not exist.
       throw new Error('Error: File not found!');
     }
 
-    // Return default or updated values
+    // Return default or updated values.
     return contents;
+  }
+  /**
+   * Read a binary file and return a Buffer of raw data.
+   *
+   * @static
+   * @public
+   * @function readBinaryAsBuffer
+   * @memberof FileReader
+   * @param {string} file - Path of file to read.
+   * @returns {string} Content of file.
+   */
+  static readBinaryAsBuffer (file) {
+    // Setup default value.
+    let contents = '';
+
+    // Attempt to find the file.
+    if (fs.existsSync(file)) {
+      // The file exists.
+      contents = fs.readFileSync(file, 'binary');
+    } else {
+      // Throw error if file does not exist.
+      throw new Error('Error: File not found!');
+    }
+
+    // Return default or updated values.
+    return Buffer.from(contents, 'binary');
   }
 }
--- test/FileReader.test.js
+++ test/FileReader.test.js
@@ -1,7 +1,7 @@
 import FileReader from '../src/FileReader.js';
 
 describe('FileReader', () => {
-  describe('#readFile()', () => {
+  describe('#read()', () => {
     it('Should throw error if file not found', () => {
       expect(() => { FileReader.read('test/FileReader/t2.txt'); }).toThrow();
     });
@@ -9,6 +9,16 @@ describe('FileReader', () => {
     it('Should read the contents of a file', () => {
       const fr = FileReader.read('test/FileReader/t1.txt');
       expect(fr).toBe('Gibberish');
+    });
+  });
+  describe('#readBinaryAsBuffer()', () => {
+    it('Should throw error if file not found', () => {
+      expect(() => { FileReader.readBinaryAsBuffer('test/FileReader/Example2.tws'); }).toThrow();
+    });
+
+    it('Should read the binary contents of a file and return a Buffer', () => {
+      const fr = FileReader.readBinaryAsBuffer('test/FileReader/Example1.tws');
+      expect(Buffer.isBuffer(fr)).toBe(true);
     });
   });
 });
