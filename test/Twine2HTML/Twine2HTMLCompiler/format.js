820816911aaadf49ea31eff5908e0c4d1853af2a
Dan Cox <dan.cox@videlais.com>
2024-02-18 23:01:34 -0500

    Adding test story format

---
+++ test/Twine2HTML/Twine2HTMLCompiler/format.js
@@ -0,0 +1,9 @@
+window.storyFormat({
+	"name": "Test Story Format",
+	"version": "1.0.0",
+	"author": "Dan Cox",
+    "description": "A test story format.",
+    "source": "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>{{STORY_NAME}}</title><style>body { font-family: Arial, sans-serif; }</style></head><body>{{STORY_DATA}}</body></html>",
+    "license": "MIT",
+    "proofing": false
+});
