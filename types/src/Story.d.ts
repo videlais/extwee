996bf18666372644c30f61beda60863caa4ccf54
Dan Cox <dan.cox@videlais.com>
2025-08-03 09:48:20 -0500

    Updating to 2.3.2

--- types/src/Story.d.ts
+++ types/src/Story.d.ts
@@ -251,5 +251,5 @@ export class Story {
     #private;
 }
 export const creatorName: "extwee";
-export const creatorVersion: "2.3.1";
+export const creatorVersion: "2.3.2";
 import Passage from './Passage.js';
