a75dc6d8e4b1a239121cb0aa30511d2ba82eaaeb
Dan Cox <dan.cox@videlais.com>
2026-05-01 19:01:39 -0500

    Moving to 2.3.16

--- src/Story.js
+++ src/Story.js
@@ -7,7 +7,7 @@ import { encode } from 'html-entities';
 const creatorName = 'extwee';
 
 // Set the creator version.
-const creatorVersion = '2.3.15';
+const creatorVersion = '2.3.16';
 
 /**
  * Story class.
